64914d12e0346cb47384c73f9c1475b38d10bc0b
Sai Venkata Ganesh Kumar Kondeti <vkondeti@gcs.com>
2024-11-18 19:39:02 +0530

    full pipeline test new

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Testing Nodejs Application with CICD Pipeline test.'));
+app.get('/', (req, res) => res.send('Testing Nodejs Application with CICD Pipeline test. Add New Test From source Code'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
